8ee3e12502696045f12e2bd4eec74015ac8026cd
ADMIN <ADMIN@10.20.160.137>
2017-10-07 16:27:31 +0200

    a modifie

---
+++ controleurs/c_etatFrais.php
@@ -0,0 +1,34 @@
+﻿<?php
+include("vues/v_sommaire.php");
+$action = $_REQUEST['action'];
+$idcomptable = $_SESSION['idcomptable'];
+switch($action){
+	case 'selectionnerMois':{
+		$lesMois=$pdo->getLesMoisDisponibles($idcomptable);
+		// Afin de sélectionner par défaut le dernier mois dans la zone de liste
+		// on demande toutes les clés, et on prend la première,
+		// les mois étant triés décroissants
+		$lesCles = array_keys( $lesMois );
+		$moisASelectionner = $lesCles[0];
+		include("vues/v_listeMois.php");
+		break;
+	}
+	case 'voirEtatFrais':{
+		$leMois = $_REQUEST['lstMois']; 
+		$lesMois=$pdo->getLesMoisDisponibles($idcomptable);
+		$moisASelectionner = $leMois;
+		include("vues/v_listeMois.php");
+		$lesFraisHorsForfait = $pdo->getLesFraisHorsForfait($idcomptable,$leMois);
+		$lesFraisForfait= $pdo->getLesFraisForfait($idcomptable,$leMois);
+		$lesInfosFicheFrais = $pdo->getLesInfosFicheFrais($idcomptable,$leMois);
+		$numAnnee =substr( $leMois,0,4);
+		$numMois =substr( $leMois,4,2);
+		$libEtat = $lesInfosFicheFrais['libEtat'];
+		$montantValide = $lesInfosFicheFrais['montantValide'];
+		$nbJustificatifs = $lesInfosFicheFrais['nbJustificatifs'];
+		$dateModif =  $lesInfosFicheFrais['dateModif'];
+		$dateModif =  dateAnglaisVersFrancais($dateModif);
+		include("vues/v_etatFrais.php");
+	}
+}
+?>
---
+++ controleurs/c_gererFrais.php
@@ -0,0 +1,50 @@
+<?php
+include("vues/v_sommaire.php");
+$idcomptable = $_SESSION['idcomptable'];
+$mois = getMois(date("d/m/Y"));
+$numAnnee =substr( $mois,0,4);
+$numMois =substr( $mois,4,2);
+$action = $_REQUEST['action'];
+switch($action){
+	case 'saisirFrais':{
+		if($pdo->estPremierFraisMois($idcomptable,$mois)){
+			$pdo->creeNouvellesLignesFrais($idcomptable,$mois);
+		}
+		break;
+	}
+	case 'validerMajFraisForfait':{
+		$lesFrais = $_REQUEST['lesFrais'];
+		if(lesQteFraisValides($lesFrais)){
+	  	 	$pdo->majFraisForfait($idcomptable,$mois,$lesFrais);
+		}
+		else{
+			ajouterErreur("Les valeurs des frais doivent être numériques");
+			include("vues/v_erreurs.php");
+		}
+	  break;
+	}
+	case 'validerCreationFrais':{
+		$dateFrais = $_REQUEST['dateFrais'];
+		$libelle = $_REQUEST['libelle'];
+		$montant = $_REQUEST['montant'];
+		valideInfosFrais($dateFrais,$libelle,$montant);
+		if (nbErreurs() != 0 ){
+			include("vues/v_erreurs.php");
+		}
+		else{
+			$pdo->creeNouveauFraisHorsForfait($idcomptable,$mois,$libelle,$dateFrais,$montant);
+		}
+		break;
+	}
+	case 'supprimerFrais':{
+		$idFrais = $_REQUEST['idFrais'];
+	    $pdo->supprimerFraisHorsForfait($idFrais);
+		break;
+	}
+}
+$lesFraisHorsForfait = $pdo->getLesFraisHorsForfait($idcomptable,$mois);
+$lesFraisForfait= $pdo->getLesFraisForfait($idcomptable,$mois);
+include("vues/v_listeFraisForfait.php");
+include("vues/v_listeFraisHorsForfait.php");
+
+?>
